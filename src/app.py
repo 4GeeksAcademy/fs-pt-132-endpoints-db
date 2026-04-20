403c5dcf9ee45f576cd1b82e6e631ea920fae06e
Javier Seiglie <96433186+jseiglie@users.noreply.github.com>
2026-04-20 18:26:33 +0000

    clase

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Planets, Posts, Profile, Favorites
+from sqlalchemy import select
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -44,6 +44,115 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+#endpoints con db
+
+@app.route("/people", methods=["GET"])
+def get_people():
+    people = db.session.execute(select(People)).scalars().all()
+    #pasamos de objeto a diccionario
+    transformed = [p.serialize() for p in people]
+  
+    return jsonify({"success": True, "data": transformed}), 200
+
+@app.route("/people", methods=["POST"])
+def add_people():
+    #extraemos del pedido la info
+    body = request.get_json() #lo pasamos a diccionario
+    #verificamos que tengamos toda la informacion
+    if not body["name"] or not body["hair_color"]:
+        return jsonify({"success": False, "msg": "missing data"}), 403
+
+    #generamos registro
+    new_people = People( #referencia a la tabla donde vamos a crear
+        name=body["name"],
+        hair_color= body["hair_color"]
+    )
+    #add para añadir los cambios
+    db.session.add(new_people)
+    #commit para guardar
+    db.session.commit()
+
+    return jsonify({"success": True, "data": new_people.serialize()}), 201
+
+#un solo registro
+@app.route("/people/byId/<int:id>", methods=["GET"])
+def get_one_people(id):
+    person = db.session.get(People, id)
+   
+    
+  #como devuelve un solo objeto, porque estamos buscando UN id, podemos aplicar directamente serialize()
+    return jsonify({"success": True, "data": person.serialize()}), 200
+
+#modificar registro
+@app.route("/people/update/<int:id>", methods=["PUT"])
+def mod_people(id):
+    person = db.session.get(People, id)
+    if not person: 
+        return jsonify({"success": False, "data": "not found"}), 404
+    body = request.get_json() #lo pasamos a diccionario
+    
+    #verificamos que tengamos toda la informacion
+    #person es un objeto
+    #body es un diccionario
+    person.name = body["name"] if body["name"] else person.name
+    if body['hair_color']:
+        person.hair_color = body['hair_color'] 
+    
+    #almacenamos cambios
+    db.session.commit()
+
+  #como devuelve un solo objeto, porque estamos buscando UN id, podemos aplicar directamente serialize()
+    return jsonify({"success": True, "data": person.serialize()}), 200
+
+
+
+#delete  registro
+@app.route("/people/delete/<int:id>", methods=["DELETE"])
+def delete_people(id):
+    person = db.session.get(People, id)
+
+    db.session.delete(person)
+    db.session.commit()
+   
+    
+  #como devuelve un solo objeto, porque estamos buscando UN id, podemos aplicar directamente serialize()
+    return jsonify({"success": True, "data": "deleted user " + str(id)}), 200
+
+#prioridad 
+#responde a princpio RESTful
+#la info va cifrada
+@app.route("/fav/new_by_body", methods=["POST"])
+def new_fav_body():
+    body = request.get_json()
+    new_fav = Favorites(
+        user_id =  body["user_id"],
+        planet_id = body["planet_id"],
+        people_id= body["people_id"]
+        )
+
+    db.session.add(new_fav)
+    db.session.commit()
+
+    return jsonify({'msg': "created"})
+
+#no se usa para post, mas bien se usa en GET
+@app.route('/fav/new/<int:user_id>/<int:planet_id>/<int:people_id>', methods=["POST"])
+def new_fav_dynamic(user_id, planet_id, people_id):
+
+    new_fav = Favorites(
+        user_id =  user_id,
+        planet_id = planet_id,
+        people_id= people_id
+    )
+
+    db.session.add(new_fav)
+    db.session.commit()
+
+    return jsonify(new_fav.serialize())
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,142 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
+#tabla de asociacion para relacion m2m
+class Favorites(db.Model):
+    __tablename__="favorites"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    #claves foraneas 
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"), nullable=True) #puede estar varcio
+    people_id: Mapped[int] = mapped_column(ForeignKey("people.id"), nullable=True) #puede estar varcio
+
+    #relacion --> tipo de relacion que tenemos
+    user: Mapped["User"] = relationship(back_populates="favorites")
+    planet: Mapped["Planets"] = relationship(back_populates="favorites")
+    people: Mapped["People"] = relationship(back_populates="favorites")
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user": {
+                "id": self.user.id
+            },
+            "planet":{
+                "id": self.planet.id,
+                "name": self.planet.name
+            },
+            "people": {
+                "id": self.people.id,
+                "name": self.people.name,
+                "hair_color": self.people.hair_color
+            }
+        }
+
+
+
 class User(db.Model):
+    __tablename__="user"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    #relacion
+    #m2m
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="user")
+    #1-1
+    profile: Mapped['Profile'] = relationship(back_populates="user")
+    #M-1
+    posts: Mapped[List["Posts"]] = relationship(back_populates='author')
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+            # relacion 1-1, se serializa directamente
+            "profile": self.profile.serialize() if self.profile else None,
+            #es un lista, se serializa utilizando un loop, ya sea map, lista comprensiva y se tiene que verificar que exista informacion
+            "posts": [post.serialize() for post in self.posts] if self.posts else None,
+            "favorites": [fav.serialize() for fav in self.favorites] if self.favorites else None
+        }
+
+
+
+class People(db.Model):
+    __tablename__="people"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(10), nullable=False)
+    
+    #relacion
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="people")
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hair_color": self.hair_color,
+            "favorites": [fav.serialize() for fav in self.favorites] if self.favorites else None
+
+        }
+
+
+class Planets(db.Model):
+    __tablename__="planets"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
+    weather: Mapped[str] = mapped_column(String(10), nullable=False)
+
+    #relacion
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="planet")
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "weather": self.weather,
+            "favorites": [fav.serialize() for fav in self.favorites] if self.favorites else None
+
+        }
+
+
+class Profile(db.Model):
+    __tablename__="profile"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    bio: Mapped[str] = mapped_column(Text)
+    #clave foranea
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    #relacion 1-1. No hay lista
+    user: Mapped['User'] = relationship(back_populates="profile") #almacena objeto que viene de tabla user
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "bio": self.bio,
+            #pasar a diccionario el objeto cuando devolvemos la informacion al endpoint
+            "user": self.user.serialize() if self.user else None
+        }
+
+class Posts(db.Model):
+    __tablename__="posts"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    #clave foranea
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    #relacion M-1
+    author: Mapped["User"] = relationship(back_populates='posts')
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body,
+            #es un author por post, no es una lista, podemos serializar directamente
+            "author": self.author.serialize() if self.author else None
         }
